cd33c64e05e26517b02690e8d9fcec73615a39b9
Shikhar <vermashikhar20@gmail.com>
2024-03-01 07:04:19 +0530

    modified:   index.jsx 	deleted:    src/assets 	new file:   src/assets/MemesData.jsx 	new file:   src/components/App.jsx 	new file:   src/components/Header.jsx 	modified:   styles.css

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Memes Generator</h1>
+            <h3>Made in React</h3>
+        </header>
+    )
+}
